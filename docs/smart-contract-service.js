d6e301b68ca471f8ec2a0d56711e146d945e5f3b
XYZ <39558321+lastrrx@users.noreply.github.com>
2025-07-11 12:04:16 +0200

    Update smart-contract-service.js

--- docs/smart-contract-service.js
+++ docs/smart-contract-service.js
@@ -37,7 +37,7 @@ class SmartContractService {
                 }
             );
             this.programId = new solanaWeb3.PublicKey(
-                window.BLOCKCHAIN_CONFIG?.SOLANA_PROGRAM_ID || 'FtYH93zU1WtfiNNwFJ7K8We4AYHC961LBN3wbPb4xVMH' 
+                window.BLOCKCHAIN_CONFIG?.SOLANA_PROGRAM_ID || '2aGet7TxaynJ5QtyzF1CP2ujT3QWvYefGLBU5Kjzk5JV' 
             );
             this.platformWallet = new solanaWeb3.PublicKey('HmT6Nj3r24YKCxGLPFvf1gSJijXyNcrPHKKeknZYGRXv');
             this.available = true;
